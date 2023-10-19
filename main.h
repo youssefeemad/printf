9be273163b23594bb0c45351fbf25a601b79c6a6
youssefeemad <eyoussef228@gmail.com>
2023-10-20 01:57:25 +0300

    new

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void print(const char *format, ...);
+int _puts(const char *str);
+int _putchar(char c);
+
+#endif
